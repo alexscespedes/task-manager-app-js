ca5bcd9e43750ac57d732edf55dfbfdce306953b
alexscespedes <alexandersenc99@gmail.com>
2025-04-24 04:44:00 -0400

    Create filter by status (all, active, completed)

--- task-manager-app.js
+++ task-manager-app.js
@@ -1,12 +1,20 @@
 const generateIDPlus = () =>
   `${Date.now()}-${Math.floor(Math.random() * 10000)}`;
 
-// const taskExample = [{
-//     id: generateIDPlus(),
-//     name: "Advanced JavaScript Exercise",
-//     completed: false,
-//     createdAt: new Date().toISOString(),
-//   }];
+const taskExample = [
+  {
+    id: generateIDPlus(),
+    name: "Advanced JavaScript Exercise",
+    completed: false,
+    createdAt: new Date().toISOString(),
+  },
+  {
+    id: generateIDPlus(),
+    name: "Basic C-Sharp Console App",
+    completed: true,
+    createdAt: new Date().toISOString(),
+  },
+];
 
 const tasks = [];
 
@@ -44,6 +52,27 @@ const toggleTaskStatus = function (taskId) {
   return task.completed;
 };
 
-const filterTasks = function (taskId) {};
+const listTasks = function () {
+  if (tasks.length === 0) {
+    return "No tasks available";
+  }
+  return tasks
+    .map(
+      (task) =>
+        `${task.completed ? "Completed" : "Active"} | ID: ${task.id}, Name: ${
+          task.name
+        } | Completed: ${task.completed} | CreatedAt: ${task.createdAt}`
+    )
+    .join("\n");
+};
 
-const searchTasks = function (taskId) {};
+const filterTasksByCompletion = function (status) {
+  if (status === undefined) {
+    return listTasks();
+  }
+  return tasks.filter((task) => task.completed === status);
+};
+
+const searchTasks = function (query) {};
+
+console.log(filterTasksByCompletion(false));
